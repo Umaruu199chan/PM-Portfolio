526a4328beac32377a031bb7243e41bda6495eba
patrick <trickmeredor@gmail.com>
2025-06-16 22:38:53 +0800

    Added Navbar

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,34 @@
 function App() {
-    return (
-        <nav className="">
-            
-        </nav>
-    )
+  return (
+    <nav className="border border-gray-500 p-0 bg-gray-50 max-w-lg rounded-full mt-5 mx-auto shadow-xl/25">
+      <ul className="flex flex-row justify-around items-center">
+        <li className="hover:scale-105 transition-transform">
+          <a href="#" className="hover:text-gray-600">
+            HOME
+          </a>
+        </li>
+        <li className="hover:scale-105 transition-transform">
+          <a href="#" className="hover:text-gray-600">
+            PROJECTS
+          </a>
+        </li>
+        <li className="hover:scale-120 transition-transform">
+          <a href="#">
+            <img src="public\pm.svg" alt="Logo" className="h-15 w-15" />
+          </a>
+        </li>
+        <li className="hover:scale-105 transition-transform">
+          <a href="#" className="hover:text-gray-600">
+            ABOUT
+          </a>
+        </li>
+        <li className="hover:scale-105 transition-transform">
+          <a href="#" className="hover:text-gray-600">
+            CONTACT ME
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
-export default App
+export default App;
